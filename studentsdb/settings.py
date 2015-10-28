3e63d78f1591e2d2c203eec7c59e064e109c6473
Yurii Onuferko <yura_onuferko@yahoo.com>
2015-10-28 11:19:40 +0200

    add students app to our proojject

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+    'students',)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
