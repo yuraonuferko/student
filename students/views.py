6fc7badc4a72f6ad2f79c48556aa65fa869dcd8a
Yurii Onuferko <yura_onuferko@yahoo.com>
2015-10-29 18:13:43 +0200

    dynamizaciija studenta

--- students/views.py
+++ students/views.py
@@ -1,10 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 # Views for Students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+    {'id': 1,
+     'first_name': u'Корост',
+     'last_name': u'Андрій',
+     'ticket': 235,
+     'image': 'img/me.jpeg'},
+    {'id': 2,
+     'first_name': u'Подоба',
+     'last_name': u'Віталій',
+     'ticket': 2123,
+     'image': 'img/piv.png'},
+    {'id': 3,
+     'first_name': u'Онуферко',
+     'last_name': u'Юрій',
+     'ticket': 3333,
+     'image': 'img/podoba3.jpg'},              
+    )              
+    return render(request, 'students/students_list.html',{'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
