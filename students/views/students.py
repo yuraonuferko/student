1e98f58c6db9c52a0244abc2f3fe12322c21bf8f
Yurii Onuferko <yura_onuferko@yahoo.com>
2015-12-02 16:38:26 +0200

    pagination

--- students/views/students.py
+++ students/views/students.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models import Student
 # Create your views here.
 
@@ -14,7 +15,20 @@ def students_list(request):
     if order_by in ('last_name', 'first_name', 'ticket','id'):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
-            students = students.reverse()
+            students = students.reverse
+    
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+            students = paginator.page(page)
+    except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+            students = paginator.page(1)
+    except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver
+                # last page of results.
+            students = paginator.page(paginator.num_pages)
 
     return render(request, 'students/students_list.html',{'students': students})
 
